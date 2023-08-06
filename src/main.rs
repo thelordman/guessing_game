a6e5af6370ba2c0009449679166d0f995424d0d4
thelordman <thegreaterlord@gmail.com>
2023-08-06 21:17:42 +0300

    Update guessing_game to version 1.0.0 with improved input handling
    Updated the version of the 'guessing_game' package from 0.1.0 to 1.0.0 in Cargo.toml. Also, improved user input handling in the main program. The updated input handling now continues the program in the case of non-numeric input, preventing a crash. Additionally, once the user guesses the number correctly, the program will exit the loop.

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,20 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u8 = guess.trim().parse().expect("Please type a number");
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("Your guess was {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
