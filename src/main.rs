cf2ec8680055104cbe85445e0188ac4504e2164c
thelordman <thegreaterlord@gmail.com>
2023-08-06 15:16:23 +0300

    Add random number generation to guessing game
    This change introduces random number generation to the guessing game. The 'rand' crate has been added as a dependency, providing the capability to generate a random number for the user to guess.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("|-------------------|");
@@ -7,7 +8,9 @@ fn main() {
 
     println!();
 
-    println!("Input your guess (0-100)");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Input your guess (1-100)");
 
     let mut guess = String::new();
 
@@ -15,5 +18,6 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("Your guess was {guess}")
+    println!("Your guess was {guess}");
+    println!("The secret number was {secret_number}");
 }
