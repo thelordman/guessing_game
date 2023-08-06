e6c1bc1b9364c6e604d8e449bb5124098e3940c3
thelordman <thegreaterlord@gmail.com>
2023-08-06 14:49:56 +0300

    Add basic user interaction to main function
    The main function has been modified to interact with the user by accepting a number input.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("|-------------------|");
+    println!("|   Guessing Game   |");
+    println!("|-------------------|");
+
+    println!();
+
+    println!("Input your guess (0-100)");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guess was {guess}")
 }
