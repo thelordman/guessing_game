1bc9ca6dceb6cfc4c64148b8b6ad6f611a83045e
thelordman <thegreaterlord@gmail.com>
2023-08-07 22:30:53 +0300

    Update guessing game to self-solving version
    Changes have been made to update the original guessing game into a self-solving one. In this version, the program automates the guessing process to zero in on the secret number. This is achieved by adjusting the guess each time based on comparing the current guess with the upper and lower bounds in the previous guesses.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,52 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
 
 fn main() {
-    println!("|-------------------|");
-    println!("|   Guessing Game   |");
-    println!("|-------------------|");
+    println!("|----------------------------------|");
+    println!("|   Guessing Game (self solving)   |");
+    println!("|----------------------------------|");
 
     println!();
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    let mut guesses_small: Vec<u32> = vec![0];
+    let mut guesses_big: Vec<u32> = vec![100];
+
+    let mut guess = 50;
+
     loop {
         println!("Input your guess (1-100)");
 
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+        println!("{guess}");
 
         println!("Your guess was {guess}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => {
+                println!("Too small!");
+
+                guesses_small.push(guess);
+                guess += (get_smallest(&guesses_big) - get_biggest(&guesses_small)) / 2
+            },
+            Ordering::Greater => {
+                println!("Too big!");
+
+                guesses_big.push(guess);
+                guess -= (get_smallest(&guesses_big) - get_biggest(&guesses_small)) / 2
+            },
             Ordering::Equal => {
                 println!("You win!");
                 break;
             }
         }
     }
+}
+
+fn get_biggest(vec: &Vec<u32>) -> u32 {
+    **vec.iter().max().unwrap()
+}
+
+fn get_smallest(vec: &Vec<u32>) -> u32 {
+    **vec.iter().min().unwrap()
 }
